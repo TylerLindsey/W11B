c1383f3595cb100cd4b8bfb01fc34d5dd23288c4
tygoli <tylerlindsey403@gmail.com>
2022-04-01 23:48:22 -0600

    added comments

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ function getQuote(){
       url : "https://inspiration.goprogram.ai"
     }).then(successFunction).catch(failureFunction);
 }
-
+// above: create a function called getQuote that assigns a value to html h2 from the URL with a GET request. then there is a reference to the funtions for success and failure, which are the 2 next functions.
 function successFunction(response){
   console.log(response);
   let quote1 = response.data.quote;
@@ -18,7 +18,7 @@ function successFunction(response){
 function failureFunction(error){
   console.log(error);
 }
-
+// below references the functions once the click is made. 
 document.getElementById('clickQuote').addEventListener('click', getQuote);
 
 
